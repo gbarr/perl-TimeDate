09a25ecca0e63db8f9eed4ba7f3d8e860307e6f8
Graham Barr <gbarr@pobox.com>
2006-12-24 13:21:44 +0000

    Correct doc for %e

--- lib/Date/Format.pm
+++ lib/Date/Format.pm
@@ -352,7 +352,7 @@ category of the program's locale.
 	%c	MM/DD/YY HH:MM:SS
 	%C 	ctime format: Sat Nov 19 21:05:57 1994
 	%d 	numeric day of the month, with leading zeros (eg 01..31)
-	%e 	numeric day of the month, without leading zeros (eg 1..31)
+	%e 	like %d, but a leading zero is replaced by a space (eg  1..32)
 	%D 	MM/DD/YY
 	%G	GPS week number (weeks since January 6, 1980)
 	%h 	month abbr
